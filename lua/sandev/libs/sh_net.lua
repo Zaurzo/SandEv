70e41697864fd2d729696d0f77599f6933c7479e
Xalalau Xubilozo <xalalau@outlook.com>
2023-01-03 13:44:24 -0300

    Add SEv.Net:SendData (untested)

--- lua/sandev/libs/sh_net.lua
+++ lua/sandev/libs/sh_net.lua
@@ -1,9 +1,16 @@
--- Send huge 
-function SEv.Net:SendString(string, callbackName, ply)
-    local chunksID = util.MD5(string)
+-- Send huge string
+function SEv.Net:SendString(str, callbackName, ply)
+    local chunksID = util.MD5(str)
+    local compressedString = util.Compress(str)
+
+    SEv.Net:SendData(chunksID, compressedString, callbackName, ply, true)
+end
+
+-- Send huge binary
+function SEv.Net:SendData(chunksID, data, callbackName, ply, isCompressedString)
     local chunksSubID = SysTime()
-    local compressedString = util.Compress(string)
-    local totalSize = string.len(compressedString)
+
+    local totalSize = string.len(data)
     local chunkSize = 64000 -- 64KB (Max value: 64KB. Min value: 11KB (so we get a min 0.1s delay on 1MBps cap speed))
     local totalChunks = math.ceil(totalSize / chunkSize)
     local maxSpeed = chunkSize / 1000 / 1024 -- 1 MBps
@@ -18,7 +25,7 @@ function SEv.Net:SendString(string, callbackName, ply)
         local startByte = chunkSize * (i - 1) + 1
         local remaining = totalSize - (startByte - 1)
         local endByte = remaining < chunkSize and (startByte - 1) + remaining or chunkSize * i
-        local chunk = string.sub(compressedString, startByte, endByte)
+        local chunk = string.sub(data, startByte, endByte)
 
         timer.Simple(i * maxSpeed, function()
             if SEv.Net.sendTab[chunksID] ~= chunksSubID then return end
@@ -31,6 +38,7 @@ function SEv.Net:SendString(string, callbackName, ply)
             net.WriteUInt(#chunk, 16)
             net.WriteData(chunk, #chunk)
             net.WriteBool(isLastChunk)
+            net.WriteBool(tobool(isCompressedString))
             if isLastChunk then
                 net.WriteString(callbackName)
             else
@@ -55,13 +63,14 @@ net.Receive("sev_net_send_string", function()
     local len = net.ReadUInt(16)
     local chunk = net.ReadData(len)
     local isLastPart = net.ReadBool()
+    local isCompressedString = net.ReadBool()
     local callbackName = net.ReadString() -- Empty until isLastPart is true.
 
     -- Initialize streams or reset overwriten ones
     if not SEv.Net.receivedTab[chunksID] or SEv.Net.receivedTab[chunksID].chunksSubID ~= chunksSubID then
         SEv.Net.receivedTab[chunksID] = {
             chunksSubID = chunksSubID,
-            compressedString = ""
+            data = ""
         }
 
         -- 3 minutes to remove possible memory leaks
@@ -71,11 +80,16 @@ net.Receive("sev_net_send_string", function()
     end
 
     -- Rebuild the compressed string
-    SEv.Net.receivedTab[chunksID].compressedString = SEv.Net.receivedTab[chunksID].compressedString .. chunk
+    SEv.Net.receivedTab[chunksID].data = SEv.Net.receivedTab[chunksID].data .. chunk
 
     -- Finish stream
     if isLastPart then
-        local decompressedString = util.Decompress(SEv.Net.receivedTab[chunksID].compressedString)
-        _G[callbackName](decompressedString)
+        local data = SEv.Net.receivedTab[chunksID].data
+
+        if isCompressedString then
+            data = util.Decompress(data)
+        end
+
+        _G[callbackName](data)
     end
 end)
